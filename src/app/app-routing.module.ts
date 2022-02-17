53b2470d24241ac102f8fd22ef89ad5751662ee1
diopmorfall <morfall.papydiop99@gmail.com>
2022-02-17 11:21:57 +0100

    Modules updated, and routes to the items implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { ItemComponent } from './components/item/item.component';
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  // { path: 'animals/:animalId', component: AnimalDetails},
+  { path: 'animals', component: ItemListComponent},
+  { path: 'animals/:animalId', component: ItemComponent},
   { path: 'about-us', component: AboutUsComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ItemComponent } from './components/item/item.component';
+import { FormatNamePipe } from './pipes/format-name.pipe';
+import { ItemListComponent } from './components/item-list/item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     NavbarComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ItemComponent,
+    FormatNamePipe,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
